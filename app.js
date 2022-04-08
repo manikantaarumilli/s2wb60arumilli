5fccf7fdbd6486def1ee7d4ba2243a694e930d6a
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-04-07 22:38:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dogRouter = require('./routes/dog');
+var jamRouter = require('./routes/jam');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dog', dogRouter);
+app.use('/jam', jamRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 
