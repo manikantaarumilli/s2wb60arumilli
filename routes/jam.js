dc72bd7c46ff89f34504775bb34e5989306ab2e8
manikantaarumilli <98128314+manikantaarumilli@users.noreply.github.com>
2022-04-07 22:48:25 -0500

    Update jam.js

--- routes/jam.js
+++ routes/jam.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('dog', { title: 'search result'});
+    res.render('jam', { title: 'search result'});
 });
 
 module.exports = router;
